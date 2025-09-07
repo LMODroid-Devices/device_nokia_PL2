fcbb7fa8f837eb4f59b33f22e5fd3660354db77b
LMO GerritBot <gerrit@libremobileos.com>
2025-09-07 13:08:02 +0000

    Adapt to LMODroid by device-add script

--- lmodroid_PL2.mk
+++ lmodroid_PL2.mk
@@ -1,6 +1,6 @@
 # Inherit some common Lineage stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lmodroid/config/common_full_phone.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Device
@@ -10,7 +10,7 @@ PRODUCT_BRAND := Nokia
 PRODUCT_DEVICE := PL2
 PRODUCT_MANUFACTURER := HMD Global
 PRODUCT_MODEL := Nokia 6.1
-PRODUCT_NAME := lineage_PL2
+PRODUCT_NAME := lmodroid_PL2
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="Plate2_00WW-user 10 QKQ1.190828.002 00WW_4_15O release-keys" \
